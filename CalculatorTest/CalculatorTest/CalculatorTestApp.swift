41807dca9b7ae96681f38c202420556cc7ddfe56
israman <israelmanzo814@gmail.com>
2022-04-29 14:34:35 -0400

    .added project

---
+++ CalculatorTest/CalculatorTest/CalculatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTestApp.swift
+//  CalculatorTest
+//
+//  Created by Israel Manzo on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorTest/CalculatorTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorTest
+//
+//  Created by Israel Manzo on 4/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
