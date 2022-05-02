7ab94c7aad866375a07b1502130d7258058704af
israman <israelmanzo814@gmail.com>
2022-05-01 21:18:25 -0400

    added order of methods

--- BasicsOfUnitTest/BasicsOfUnitTestTests/BasicsOfUnitTestTests.swift
+++ BasicsOfUnitTest/BasicsOfUnitTestTests/BasicsOfUnitTestTests.swift
@@ -8,14 +8,38 @@
 import XCTest
 @testable import BasicsOfUnitTest
 
+/// Unit Test runs in alphabetical order - see below
+/**
+ Running order
+ 1 -> setUpWithError()
+ 2 -> testExample()
+ 3 -> tearDownWithError()
+ 4 -> testPerformanceExample()
+ 
+ Runing order when custom testing method exist
+ 1 -> setUpWithError()
+ 2 -> testA()
+ 3 -> tearDownWithError()
+ */
+
 class BasicsOfUnitTestTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("1 - Set up with error")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        print("3 - Tear down with error")
+    }
+    
+    func testB() {
+        print("TEST B")
+    }
+    
+    func testA() {
+        print("TEST A")
     }
 
     func testExample() throws {
@@ -24,12 +48,14 @@ class BasicsOfUnitTestTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        print("2 - Test Example")
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            print("4 - Test Perform exmaple - measure")
         }
     }
 
