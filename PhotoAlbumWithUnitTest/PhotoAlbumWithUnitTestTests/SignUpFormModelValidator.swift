1195d774c9312f594435c0500eafc05fb17daf3e
israman <israelmanzo814@gmail.com>
2022-05-02 21:01:09 -0400

    added new unit test bundle + new test file

---
+++ PhotoAlbumWithUnitTest/PhotoAlbumWithUnitTestTests/SignUpFormModelValidator.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpFormModelValidator.swift
+//  PhotoAlbumWithUnitTestTests
+//
+//  Created by Israel Manzo on 5/2/22.
+//
+
+import XCTest
+
+class SignUpFormModelValidator: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+
+}
