05bad771d46dcbd93324d4bfe0eaf08e7182e48c
israman <israelmanzo814@gmail.com>
2022-05-02 20:50:53 -0400

    added new project for using TDD login screen

---
+++ PhotoAlbumWithUnitTest/PhotoAlbumWithUnitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoAlbumWithUnitTest
+//
+//  Created by Israel Manzo on 5/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
