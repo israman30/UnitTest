25ac3e1bb723bbd55405925d94594d2dca52a50d
Israel Manzo <israelmanzo814@gmail.com>
2022-12-20 22:12:51 -0500

    Added new project for Unit Testing

---
+++ Network Request/Network Request/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Network Request
+//
+//  Created by Israel Manzo on 12/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Network Request/Network Request/Network_RequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_RequestApp.swift
+//  Network Request
+//
+//  Created by Israel Manzo on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Network_RequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
