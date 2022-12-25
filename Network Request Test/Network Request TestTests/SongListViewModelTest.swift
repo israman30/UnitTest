aef865ed3b41255d235a3e2db33c4f7bf4ab055c
Israel Manzo <israelmanzo814@gmail.com>
2022-12-24 22:18:34 -0500

    added new test class file for network request

---
+++ Network Request Test/Network Request TestTests/SongListViewModelTest.swift
@@ -0,0 +1,22 @@
+//
+//  SongListViewModelTest.swift
+//  Network Request TestTests
+//
+//  Created by Israel Manzo on 12/24/22.
+//
+
+import XCTest
+@testable import Network_Request_Test
+
+final class SongListViewModelTest: XCTestCase {
+    
+    var songListViewModel: SongListViewModel!
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
