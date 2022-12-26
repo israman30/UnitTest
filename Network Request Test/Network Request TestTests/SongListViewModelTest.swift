9f20c485d43ac915e0b7b538f631e8efe7682f40
Israel Manzo <israelmanzo@Israels-MacBook-Pro-2.local>
2022-12-25 21:38:06 -0500

    updating network request test using combine and subcriber for listening changes in songs array

--- Network Request Test/Network Request TestTests/SongListViewModelTest.swift
+++ Network Request Test/Network Request TestTests/SongListViewModelTest.swift
@@ -6,25 +6,40 @@
 //
 
 import XCTest
+import Combine
 @testable import Network_Request_Test
 
 final class SongListViewModelTest: XCTestCase {
     
     var songListViewModel: SongListViewModel!
+    private var cancellable: Set<AnyCancellable>!
 
     override func setUp() {
         super.setUp()
         songListViewModel = SongListViewModel(httpClient: MockHttpClient())
+        cancellable = []
     }
     
     override func tearDown() {
         super.tearDown()
         songListViewModel = nil
+        cancellable = []
     }
     
     func test_fecthSong_Successfully() async throws {
+        let expectation = XCTestExpectation(description: "Fetched Songs")
+        
         try await songListViewModel.fetchSongs()
         
-        XCTAssertEqual(songListViewModel.songs.count, 2)
+        songListViewModel
+            .$songs
+            .dropFirst()
+            .sink { value in
+                XCTAssertEqual(value.count, 2)
+                expectation.fulfill()
+            }
+            .store(in: &cancellable)
+        
+        wait(for: [expectation], timeout: 1)
     }
 }
