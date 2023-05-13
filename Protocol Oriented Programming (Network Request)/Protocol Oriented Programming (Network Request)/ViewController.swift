888d3acef21320c70a8441bec46d90fef05c51e2
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-05-13 12:27:15 -0400

    added viewmodel

--- Protocol Oriented Programming (Network Request)/Protocol Oriented Programming (Network Request)/ViewController.swift
+++ Protocol Oriented Programming (Network Request)/Protocol Oriented Programming (Network Request)/ViewController.swift
@@ -7,7 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol ViewModelProtocol: AnyObject {
+    func updateView(imageUrl: String, email: String)
+}
+
+class ViewModel {
+    
+    weak var viewModelProtocol: ViewModelProtocol?
+    
+    private let networkManager: NetworkManager
+    
+    init(_ networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchUsers() {
+        NetworkManager.shared.getUser { [weak self] user in
+            switch user {
+            case .success(let user):
+                self?.viewModelProtocol?.updateView(imageUrl: user.avatar, email: user.email)
+            case .failure(let error):
+                print(error.localizedDescription)
+                self?.viewModelProtocol?.updateView(imageUrl: "placeholder", email: "no user found")
+            }
+        }
+    }
+}
+
+class ViewController: UIViewController, ViewModelProtocol {
     
     let imagePhoto: UIImageView = {
         let iv = UIImageView()
@@ -22,11 +49,28 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
+    private let vm: ViewModel
+    
+    init(vm: ViewModel) {
+        self.vm = vm
+        super.init(nibName: nil, bundle: nil)
+        vm.viewModelProtocol = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupView()
+        fetchUsers()
+    }
+    
+    func updateView(imageUrl: String, email: String) {
+        
     }
     
     func setupView() {
@@ -45,7 +89,50 @@ class ViewController: UIViewController {
             titleLabel.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
+    
+    private func fetchUsers() {
+        NetworkManager.shared.getUser { user in
+            switch user {
+            case .success(let user):
+                self.titleLabel.text = user.email
+                DispatchQueue.main.async {
+                    let imageData = try! NSData(contentsOf: URL(string: user.avatar)!) as Data
+                    self.imagePhoto.image = UIImage(data: imageData)
+                }
+            case .failure(let error):
+                self.titleLabel.text = "\(error.localizedDescription)"
+                self.imagePhoto.image = UIImage(named: "placeholder")
+            }
+        }
+    }
 
 
+}
+struct UserResonse: Decodable {
+    let data: User
+}
+struct User: Decodable {
+    let id: Int
+    let email: String
+    let avatar: String
+}
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func getUser(completion: @escaping(Result<User, Error>)->()) {
+        let url = URL(string: "https://reqres.in/api/users/2")!
+        URLSession.shared.dataTask(with: url) { data, res, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                if let user = try? JSONDecoder().decode(UserResonse.self, from: data).data {
+                    completion(.success(user))
+                } else {
+                    completion(.failure(error!))
+                }
+            }
+        }.resume()
+    }
 }
 
