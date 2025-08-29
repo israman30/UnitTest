106a2ac5e140ad58de30f250ffa6478bdf4d8011
Israel Manzo <israelmanzo814@gmail.com>
2025-08-28 23:09:15 -0400

    added catalog

--- Clean Unit Test/Clean Unit Test/ContentView.swift
+++ Clean Unit Test/Clean Unit Test/ContentView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+enum CatalogError: Error, Equatable {
+    case invalidPage
+    case pageOutOfBounds
+    case missingContent
+}
+
+struct Catalog {
+    let maxPages: Int
+    let content: [String]
+    func content(for page: Int) throws -> String {
+        guard page >= 0 else { throw CatalogError.invalidPage }
+        guard page < maxPages else { throw CatalogError.pageOutOfBounds }
+        guard page < content.count else { throw CatalogError.missingContent }
+        
+        return content[page]
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
